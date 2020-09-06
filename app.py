280cc138498bd800809cc087f971c9eba6c0c8b4
ArcaneAlloy <50646302+ArcaneAlloy@users.noreply.github.com>
2020-09-06 07:49:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-from PhishyClf/PhishyClf import make_pred
+from PhishyClf.PhishyClf import *
 
 app = Flask(__name__)
 CORS(app)
