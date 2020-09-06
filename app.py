9214847c7c5ddf24700187b12e919f64ffd6a2e3
ArcaneAlloy <50646302+ArcaneAlloy@users.noreply.github.com>
2020-09-06 07:06:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
+from PhishyClf import main
 
 app = Flask(__name__)
 CORS(app)
@@ -9,9 +10,7 @@ def hello():
 
     # POST request
     if request.method == 'POST':
-        print('Incoming..')
-        print(request.get_json())  # parse as JSON
-        return 'OK', 200
+        return main(), 200
 
     # GET request
     else:
